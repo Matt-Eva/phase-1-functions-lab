f6c7adbac761c2a5232c836d3fd6546c01ab82e4
Matt Eva <matteweva@gmail.com>
2021-10-12 15:27:38 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+// you can use Math.abs() to return the absolute value of a difference.
+const hq = 42;
+
+function distanceFromHqInBlocks(address){
+    if (address > hq) {
+        return address - hq;
+    } else {
+        return hq - address;
+    }
+}
+
+function distanceFromHqInFeet(address){
+   return distanceFromHqInBlocks(address)*264;
+}
+
+function distanceTravelledInFeet(address1, address2){
+    if (address1 > address2) {
+        return (address1 - address2) * 264;
+    } else {
+        return (address2 - address1) * 264;
+    }
+ }
+
+ function calculatesFarePrice(address1, address2) {
+    let travel = distanceTravelledInFeet(address1, address2);
+    if (travel < 400) {
+        return 0;
+    } else if (travel < 2000 ) {
+        return ((travel - 400) * .02);
+    } else if (travel < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+ }
+
